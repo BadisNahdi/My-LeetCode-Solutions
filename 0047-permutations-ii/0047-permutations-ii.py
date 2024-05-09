74f1405c72d7ced5a03f11a58ea53560a5872f95
BadisNahdi <104691724+BadisNahdi@users.noreply.github.com>
2024-05-09 21:57:12 +0100

    Time: 815 ms (5.05%), Space: 12.2 MB (13.48%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        res = []
+        sol = []
+        used = []
+        def dfs():
+            if (len(sol) == len(nums)) and (sol[:] not in res):
+                res.append(sol[:])
+            for i in range(len(nums)):
+                if(i not in used):
+                    sol.append(nums[i])
+                    used.append(i)
+                    dfs()
+                    used.pop()
+                    sol.pop()
+                    
+        dfs()
+        return res
+                    
+        
